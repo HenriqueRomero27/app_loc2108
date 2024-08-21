7b98f6f1755fbcd12df27aefe43028c566ef25e9
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-08-21 17:49:33 +0000

    test

---
+++ loc/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, View } from 'react-native';
+import Home from "./telas/Home"
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Home />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
